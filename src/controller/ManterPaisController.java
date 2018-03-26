11ccdda226f1559153ab2316699d0bb1d59f1821
Pablo Tamburu <pablotamburu123@gmail.com>
2018-03-25 22:00:41 -0300

    aula 04

---
+++ src/controller/ManterPaisController.java
@@ -0,0 +1,48 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Pais;
+import service.PaisService;
+
+/**
+ * Servlet implementation class ManterPaisController
+ */
+@WebServlet("/ManterPais.do")
+public class ManterPaisController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	doPost(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String pNome = request.getParameter("nome");
+	String pPopu = request.getParameter("populacao");
+	long pPopulacao = Long.parseLong(pPopu);
+	double pArea = Double.parseDouble(request.getParameter("area"));
+	
+	//instanciar o javabean
+	Pais pais = new Pais();
+	pais.setNome(pNome);
+	pais.setPopulacao(pPopulacao);
+	pais.setArea(pArea);
+	//instanciar o service
+	PaisService ps = new PaisService();
+	ps.criar(pais);
+	pais = ps.carregar(pais.getId());
+	PrintWriter out = response.getWriter();
+	out.println("<html><head><title>Pais Cadastrado</title></head><body>");
+	out.println( "id: "+pais.getId()+"<br>");
+	out.println( "nome: "+pais.getNome()+"<br>");
+	out.println( "populacao: "+pais.getPopulacao()+"<br>");
+	out.println( "area: "+pais.getArea()+"<br>");
+	out.println("</body></html>");
+	}
+	}
---
+++ src/test/PaisTest.java
@@ -0,0 +1,111 @@
+package test;
+
+
+import static org.junit.Assert.assertEquals;
+
+import model.Pais;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import service.PaisService;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class PaisTest {
+	Pais pais, copia;
+	PaisService paisService;
+	static int id = 0;
+
+	/*
+	 * Antes de rodar este teste, certifique-se que nao ha no banco nenhuma
+	 * linha com o id igual ao do escolhido para o to instanciado abaixo. Se
+	 * houver, delete. 
+	 * Certifique-se também que sobrecarregou o equals na classe
+	 * Cliente. 
+	 * Certifique-se que a fixture cliente1 foi criada no banco.
+	 * Além disso, a ordem de execução dos testes é importante; por
+	 * isso a anotação FixMethodOrder logo acima do nome desta classe
+	 */
+	@Before
+	public void setUp() throws Exception {
+		System.out.println("setup");
+		pais = new Pais();
+		pais.setId(id);
+		pais.setNome("Alemanha");
+		pais.setPopulacao(82521700L);
+		pais.setArea(3570510.0);
+		copia = new Pais();
+		copia.setId(id);
+		copia.setNome("Alemanha");
+		copia.setPopulacao(82521700L);
+		copia.setArea(3570510.0);
+		paisService = new PaisService();
+		System.out.println(pais);
+		System.out.println(copia);
+		System.out.println(id);
+	}
+	
+	@Test
+	public void test00Carregar() {
+		System.out.println("carregar");
+		//para funcionar o cliente 1 deve ter sido carregado no banco por fora
+		Pais fixture = new Pais();
+		fixture.setId(1);
+		fixture.setNome("Belgica");
+		fixture.setPopulacao(10403951);
+		fixture.setArea(30528);
+		PaisService novoService = new PaisService();
+		Pais novo = novoService.carregar(130);
+		assertEquals("testa inclusao", novo, fixture);
+	}
+
+	@Test
+	public void test01Criar() {
+		System.out.println("criar");
+		id = paisService.criar(pais);
+		System.out.println(id);
+		copia.setId(id);
+		assertEquals("testa criacao", pais, copia);
+	}
+
+	@Test
+	public void test02Atualizar() {
+		System.out.println("atualizar");
+		pais.setPopulacao(999999);
+		copia.setPopulacao(999999);		
+		paisService.atualizar(pais);
+		pais = paisService.carregar(pais.getId());
+		assertEquals("testa atualizacao", pais, copia);
+	}
+
+	@Test
+	public void test03Excluir() {
+		System.out.println("excluir");
+		copia.setId(-1);
+		copia.setNome(null);
+		copia.setPopulacao(0);
+		copia.setArea(0);
+		paisService.excluir(id);
+		pais = paisService.carregar(id);
+		assertEquals("testa exclusao", pais, copia);
+	}
+	
+	
+	@Test
+	public void test04MaiorPopulacao() {
+		System.out.println("maior populacao");
+		pais.getPopulacao();
+		copia.getPopulacao();
+		assertEquals("testa busca", pais, copia);
+	}
+	
+	@Test
+	public void test05MenorArea() {
+		System.out.println("menor area");
+		pais.getArea();
+		copia.getArea();
+		assertEquals("testa busca", pais, copia);
+	}
+}
